cd696b1fde52eb9257a0e2a950c16e48f7643288
danielswei <123605404+danielswei@users.noreply.github.com>
2023-11-28 15:19:24 -0600

    Update SearchBar.js to take query and send user to url

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Papa from 'papaparse';
 
 function SearchBar() {
+  // Search for a direct line like for query: Daniel Swei = https://directory.tamu.edu/?branch=people&cn=daniel+swei
+  const handleSearch = () => {
+    const query_name = document.getElementById('professorSearch').value;
+    const url = `https://directory.tamu.edu/?branch=people&cn=${encodeURIComponent(query_name)}`;
+    window.location.href = url;
+  };
   return (
     <div style={{ marginTop: '50px', textAlign: 'center' }}>
-      <input type="text" placeholder="Search for professors..." style={{ padding: '10px', borderRadius: '5px', width: '300px', border: '2px solid #500000' }} />
-      <button style={{ marginLeft: '10px', padding: '10px 20px', backgroundColor: '#700000', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+      <input type="text" placeholder="Search for professors..." id="professorSearch" style={{ padding: '10px', borderRadius: '5px', width: '300px', border: '2px solid #500000' }} />
+      <button onClick={handleSearch} style={{ marginLeft: '10px', padding: '10px 20px', backgroundColor: '#700000', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
         Search
       </button>
     </div>
