cfd09eb7d71325782c34016a47e7a9f10e4ca1b4
binhdocao <binhdocao@tamu.edu>
2023-11-30 15:44:12 -0600

    setup backend to mongodb

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import Footer from './components/Footer';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+
+
 
 function App() {
+  
+  const professorNames = {}
+
+
+
   return (
     <div className="App">
       <Header />
