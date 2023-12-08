28d79b43ca5097372db013de4aa6979cfeb9235c
binhdocao <binhdocao@tamu.edu>
2023-12-07 21:35:51 -0600

    serverless

--- api/professors/[id].js
+++ api/professors/[id].js
@@ -1,33 +1,35 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ObjectId, ServerApiVersion } = require('mongodb');
 
-const uri = process.env.MONGODB_URI;
+// MongoDB URI and Client Setup
+const uri = "mongodb+srv://aggieStudent:howdy@aggiedata.pvz7zwy.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1 } });
 
-module.exports = async (req, res) => {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
-
-  const professorId = req.query.id;
-  if (!professorId) {
-    return res.status(400).json({ error: 'Missing professor ID' });
-  }
-
-  try {
+async function getProfessorById(professorId) {
     await client.connect();
     const db = client.db("AggieProfs");
     const professors = db.collection("professors");
-    const professor = await professors.findOne({ _id: new ObjectId(professorId) });
+    return await professors.findOne({ _id: new ObjectId(professorId) });
+}
 
-    if (!professor) {
-      return res.status(404).json({ error: "Professor not found" });
+module.exports = async (req, res) => {
+    const professorId = req.query.id;
+
+    if (!professorId) {
+        return res.status(400).json({ error: "Missing professor ID" });
     }
 
-    res.status(200).json(professor);
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ error: e.message });
-  } finally {
-    await client.close();
-  }
+    try {
+        const professor = await getProfessorById(professorId);
+
+        if (!professor) {
+            return res.status(404).json({ error: "Professor not found" });
+        }
+
+        res.json(professor);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: e.message });
+    } finally {
+        await client.close();
+    }
 };
